c202263af96425896dcb5ce06bcbe6025099f507
Ron Kurr <kurr@kurron.org>
2018-04-14 11:19:36 -0400

    Added support for VWware

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ export PACKER_LOG_PATH=packer-log.txt
 PACKER_FILE=${1:-packer.json}
 
 packer validate ${PACKER_FILE}
-packer build --force ${PACKER_FILE}
+packer build --force --only vmware-vmx ${PACKER_FILE}
 vagrant box add --clean --force --name bionic-xubuntu vagrant/bionic-xubuntu.box
 vagrant box list

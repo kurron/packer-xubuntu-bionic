8e4a27945e466ffb35fdc5a00b3dc5cb5b09463b
Ron Kurr <kurr@kurron.org>
2018-04-15 11:51:01 -0400

    Post 3 covered

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 export PACKER_LOG=activate
 export PACKER_LOG_PATH=packer-log.txt
 
+# use this a the version tag
+UNIXTIME=$(date +%s)
+
 PACKER_FILE=${1:-packer.json}
 
 packer validate ${PACKER_FILE}
-packer build --force --only vmware-vmx ${PACKER_FILE}
+packer build --force ${PACKER_FILE}
 vagrant box add --clean --force --name bionic-xubuntu vagrant/bionic-xubuntu.box
 vagrant box list

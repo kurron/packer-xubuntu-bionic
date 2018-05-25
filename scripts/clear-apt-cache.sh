7aac5b540dc86f5cd9ca96545850da1eb7b395c6
Ron Kurr <kurr@jvmguy.com>
2018-05-25 14:34:17 -0400

    Remove old shell scripts

--- scripts/clear-apt-cache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash -x
-
-# Packer runs this via sudo
-
-# sometimes the background update process runs and captures the lock
-until apt-get -y update; do echo "Waiting for apt lock"; sleep 5; done
-
-echo 'Clearing APT cache...'
-apt-get clean
--- scripts/zero-out-disk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -x
-
-# this script will run via sudo
-
-echo 'Zeroing device to make space...'
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-sync

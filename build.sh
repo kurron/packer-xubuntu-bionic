035095ee215c81653fe3eedb0a9d00ed0367d442
Ron Kurr <kurr@jvmguy.com>
2018-08-04 13:54:38 -0400

    Minor improvements.

--- build.sh
+++ build.sh
@@ -10,18 +10,24 @@ export PACKER_LOG_PATH=packer-log.txt
 # use this a the version tag
 UNIXTIME=$(date +%s)
 
-PACKER_FILE=${1:-packer.json}
+PACKER_FILE=${2:-packer.json}
 
-packer validate ${PACKER_FILE}
+VALIDATE="packer validate ${PACKER_FILE}"
+echo ${VALIDATE}
+${VALIDATE}
 
-packer build --parallel=false --force ${PACKER_FILE}
+# build all platforms
+BUILD="packer build --parallel=false --force ${PACKER_FILE}"
+echo ${BUILD}
+${BUILD}
 
+# build only VMWare
 #packer build --force --only vmware-vmx ${PACKER_FILE}
 vagrant box add --clean --force --name bionic-xubuntu --provider vmware_desktop vagrant/xubuntu-bionic-vmware.box
 
+# build only VirtualBox
 #packer build --force --only virtualbox-ovf ${PACKER_FILE}
-#vagrant box add --clean --force --name bionic-xubuntu --provider virtualbox vagrant/xubuntu-bionic-virtualbox.box
-
+vagrant box add --clean --force --name bionic-xubuntu --provider virtualbox vagrant/xubuntu-bionic-virtualbox.box
 
 vagrant box list
 ls -alh vagrant
--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+function wait_for_apt_lock() {
+    until sudo apt-get --yes update; do echo "Waiting for apt lock"; sleep 15; done
+}
+
 # there are numerous ways to install Ansible but let's use the package manager
 echo "Installing Ansible via package manager..."
 
@@ -7,17 +11,22 @@ echo "Installing Ansible via package manager..."
 sudo systemctl stop apt-daily.timer
 
 # I've had issues with something grabbing the APT lock so we check at each step now
-until sudo apt-get --yes update; do echo "Waiting for apt lock"; sleep 5; done
+sleep 1m
+
+wait_for_apt_lock
 sudo apt-get update
+wait_for_apt_lock
 sudo apt-get --yes install software-properties-common
 
 echo Adding Ansible PPA
+wait_for_apt_lock
 sudo apt-add-repository ppa:ansible/ansible
 
-until sudo apt-get --yes update; do echo "Waiting for apt lock"; sleep 5; done
 echo "Who owns the lock?"
 sudo lsof /var/lib/dpkg/lock
+wait_for_apt_lock
 sudo apt-get update
+wait_for_apt_lock
 sudo apt-get --yes install ansible
 
 ansible --version
